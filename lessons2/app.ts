7402c8e298c7ae296ddade9b40c93ca6870a7874
NadiiaHonchar <17nmg82@gmail.com>
2023-07-05 18:09:52 +0300

    added lessons2.2

--- lessons2/app.ts
+++ lessons2/app.ts
@@ -58,3 +58,18 @@ const service = {
 if(service.status===Toggle.ENABLE){
     console.log('It is active');
 }
+
+let union: number | string;
+union=10;
+union='fgf';
+
+function combine(param1: number | string, param2: number | string) {
+    if (typeof param1 === 'number' && typeof param2 === 'number') {
+      return param1 + param2;
+    } else {
+      return param1.toString() + param2.toString();
+    }
+  }
+
+  let newactive: 'start' | 'end';
+  newactive= 'start';
